b4811369bcfe5690239b3a8e53261cf5490b1bc6
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-02-16 13:29:58 -0500

    print result before and after roll the dice.

--- DiceGame.kt
+++ DiceGame.kt
@@ -2,3 +2,41 @@
 // Student ID:-  A00272251
 /*  Program Description: This is small kotlin command line program based on classes and objects. In this program, I am going to create 
                          dice using contructor of Die class and display results. */
+
+class DiceGame {
+
+    fun run() {
+        // Create Dice
+        println("Creating a default d6...")
+        val d6 = Die()
+
+        println("Creating a d20...")
+        val d22 = Die(22)
+
+        println("Creating Tetrahedron die (a special d4)...")
+        val d10 = Die(10, "Tetrahedron")
+
+        // Printing the cureent side up for all newly created dice before rolling.
+        println("The current side up for ${d6.type} is ${d6.currentSide}")
+        println("The current side up for ${d20.type} is ${d20.currentSide}")
+        println("The current side up for ${d10.type} is ${d10.currentSide}")
+
+        println("Testing the roll method")
+
+        // Roll Dice
+        println("Rolling the ${d6.type}...")
+        d6.roll()
+
+        println("Rolling the ${d20.type}...")
+        d20.roll()
+
+        println("Rolling the ${d10.type}...")
+        d10.roll()
+
+        // Printing the cureent side up for all newly created dice after rolling.
+        println("New current side up for ${d6.type} is ${d6.currentSide}")
+        println("New current side up for ${d20.type} is ${d20.currentSide}")
+        println("New current side up for ${d10.type} is ${d10.currentSide}")
+
+    }
+}
--- Die.kt
+++ Die.kt
@@ -1,15 +1,19 @@
 import kotlin.random.Random
 
-class Die (var type: String = "d6", var sides: Int = 6, var currentSide: Int = 3){
+class Die (var type: String = "d6", var sides: Int = 6, var currentSide: Int = 1){
 
+    // 0 argument constructor
     init() {
         roll()
     }
 
-    contructor(sides: Int) : this("d$sides", sides)
+    // 1 argument constructor
+    constructor(sides: Int) : this("d$sides", sides)
 
-    construcor(sides: Int, type: String) : this(type, sides, 1)
+    // 2 argument constructor
+    constructor(sides: Int, type: String) : this(sides, type, 1)
 
+    // Function to get Random integer and set it to currentSide.
     fun roll() {
         currentSide = Random.nextInt(1, sides + 1)
     }
