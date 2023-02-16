f969743e0080708d0f18046116c123f0ba273b91
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-02-16 12:52:49 -0500

    Added name, studentId and description in DiceGame.kt file and class Die created in another one and also, addede 0 argument contructor.

--- DiceGame.kt
+++ DiceGame.kt
@@ -1,1 +1,4 @@
-
+// Foramben Jayeshkumar Patel
+// Student ID:-  A00272251
+/*  Program Description: This is small kotlin command line program based on classes and objects. In this program, I am going to create 
+                         dice using contructor of Die class and display results. */
--- Die.kt
+++ Die.kt
@@ -1,1 +1,12 @@
+import kotlin.random.Random
 
+class Die (var type: String = "d6", var sides: Int = 6, var currentSide: Int = 3){
+
+    init() {
+        roll()
+    }
+
+    fun roll() {
+        currentSide = Random.nextInt(1, sides + 1)
+    }
+}
