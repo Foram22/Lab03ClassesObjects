5120d208da19472c61ab02d1168935577207637a
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-02-16 13:00:33 -0500

    another two constructor created in Die.kt file.

--- Die.kt
+++ Die.kt
@@ -6,6 +6,10 @@ class Die (var type: String = "d6", var sides: Int = 6, var currentSide: Int = 3
         roll()
     }
 
+    contructor(sides: Int) : this("d$sides", sides)
+
+    construcor(sides: Int, type: String) : this(type, sides, 1)
+
     fun roll() {
         currentSide = Random.nextInt(1, sides + 1)
     }
