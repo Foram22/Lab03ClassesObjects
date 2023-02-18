9257f335dc8ab84968cc5d494fe7c227cf63fbdd
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-02-17 22:12:37 -0500

    error solved and changes done.

--- DiceGame.kt
+++ DiceGame.kt
@@ -3,48 +3,51 @@
 /*  Program Description: This is small kotlin command line program based on classes and objects. In this program, I am going to create 
                          dice using contructor of Die class and display results. */
 
-import Die 
 class DiceGame {
 
     fun run() {
         // Create Dice
         println("Creating a default d6...")
         val d6 = Die()
-
-        println("Creating a d20...")
-        val d22 = Die(22)
-
-        println("Creating Tetrahedron die (a special d4)...")
-        val d10 = Die(10, "Tetrahedron")
-
-        // Printing the cureent side up for all newly created dice before rolling.
+        // Printing the cureent side up for newly created dice d6 before rolling.
         println("The current side up for ${d6.type} is ${d6.currentSide}")
-        println("The current side up for ${d20.type} is ${d20.currentSide}")
+
+        // Create Dice
+        println("\nCreating a d22...")
+        val d22 = Die(22)
+        // Printing the cureent side up for newly created dice d22 before rolling.
+        println("The current side up for ${d22.type} is ${d22.currentSide}")
+
+        // Create Dice
+        println("\nCreating Tetrahedron die (a special d4)...")
+        val d10 = Die(10, "Tetrahedron")
+        // Printing the cureent side up for newly created dice Tetrahedron before rolling.
         println("The current side up for ${d10.type} is ${d10.currentSide}")
 
-        println("Testing the roll method")
+        println("\n\nTesting the roll method...")
 
         // Roll Dice
         println("Rolling the ${d6.type}...")
         d6.roll()
-
-        println("Rolling the ${d20.type}...")
-        d20.roll()
-
-        println("Rolling the ${d10.type}...")
-        d10.roll()
-
-        // Printing the cureent side up for all newly created dice after rolling.
+        // Printing the cureent side up for dice d6 after rolling.
         println("New current side up for ${d6.type} is ${d6.currentSide}")
-        println("New current side up for ${d20.type} is ${d20.currentSide}")
+
+        println("\nRolling the ${d22.type}...")
+        d22.roll()
+        // Printing the cureent side up for dice d22 after rolling.
+        println("New current side up for ${d22.type} is ${d22.currentSide}")
+
+        println("\nRolling the ${d10.type}...")
+        d10.roll()
+        // Printing the cureent side up for dice Tetrahedron after rolling.
         println("New current side up for ${d10.type} is ${d10.currentSide}")
 
         // Set the cureent side of dice d20 to 20
-        println("Setting the ${d20.type} to show 20...")
-        d20.currentSide = 20
-        println("The side up is now ${d20.currentSide}.")
+        println("\n\nSetting the ${d22.type} to show 22...")
+        d22.currentSide = 22
+        println("The side up is now ${d22.currentSide}.")
 
-        println("Creating five d6...")
+        println("\n\nCreating five d6...")
         val dice = Array(5) {
             Die()
         }
--- Die.kt
+++ Die.kt
@@ -4,31 +4,32 @@ class Die {
 
     var type: String = "d6"
     var sides: Int = 6
-    var currentSide: Int = roll()
+    var currentSide = 1
+    // var currentSide = roll()
 
     // 0 argument constructor
     constructor() {
         this.type = "d6"
         this.sides = 6
-        this.currentSide = roll()
+        roll()
     }
 
     // 1 argument constructor
     constructor(sides: Int) {
         this.type = "d$sides"
         this.sides = sides
-        this.currentSide = roll()
+        roll()
     }
 
     // 2 argument constructor
     constructor(sides: Int, type: String) {
         this.type = type
         this.sides = sides
-        this.currentSide = roll()
+        roll()
     }
 
     // Function to get Random integer and set it to currentSide.
     fun roll() {
-        currentSide = Random.nextInt(1, sides + 1)
+        currentSide = Random.nextInt(1, this.sides + 1)
     }
 }
